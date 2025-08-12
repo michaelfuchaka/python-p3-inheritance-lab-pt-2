588cd4992ea9b4bc6984b0dd23cfca806df6bb92
michaelfuchaka <fuchakamichael06@gmail.com>
2025-08-12 22:51:27 +0300

    Implement hello and raise_hand methods in Student and ChattyStudent classes

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,22 @@
 class Student:
-    pass
+        def hello(self):
+         print("Hey there! I'm so excited to learn stuff.")
+
+        def raise_hand(self):
+         print("Pick me!")
+        pass
 
 class ChattyStudent(Student):
-    pass
+      def hello(self):
+        # Call Student's hello() first
+        super().hello()
+        # Add extra chatty message
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! "
+              "What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+      def raise_hand(self):
+        # Call super() 10 times to print "Pick me!" 10 times
+        for _ in range(10):
+            super().raise_hand()
+      pass
